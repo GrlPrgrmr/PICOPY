553044774f3f1522a0778e7f00967901fe248afd
Neha261289 <neha.bpit@gmail.com>
2016-07-19 06:03:18 -0700

    main files

---
+++ CopyLogic.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace PICOPY
+{
+    public class CopyLogic
+    {
+        private static string filePath; // variable to hold path of file
+        private static string m_CurrentFile;
+        protected static List<string> files; // list that will hold file names to copy
+
+
+        public CopyLogic(string desPath)
+        {
+            filePath = desPath;
+
+        }
+
+
+        public static string p_filepath
+        {
+
+            get { return filePath; }
+        }
+
+        public static string mp_CurrentFile
+        {
+            get
+            {
+                return m_CurrentFile;
+            }
+            set
+            {
+                m_CurrentFile = value;
+            }
+        }
+
+        //Copy the file to destination folder using finalpath where file need to be copied
+        public static void Copy()
+        {
+
+
+            string _finalPath;
+            _finalPath = filePath;
+
+            var filename = m_CurrentFile.Substring(m_CurrentFile.LastIndexOf("\\") + 1); //extracting filename from the full path of source
+
+            if (System.IO.Directory.Exists(_finalPath))
+            {
+                _finalPath = System.IO.Path.Combine(_finalPath, filename); // combining source folder path with file name to be copied
+
+                System.IO.File.Copy(m_CurrentFile, _finalPath, true);
+            }
+
+
+        }
+
+
+        //parameters as filename will be passed
+        public static void Current(string file)
+        {
+            //hold the current displayed file
+            m_CurrentFile = file;
+
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace PICOPY
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private static List<string> fileName;
+        //this will hold the name of destination folder
+        private string _dfolderName = "";
+        private static int m_CurrentItemIndex = 0;
+        public MainWindow()
+        {
+            fileName = new List<string>();
+            InitializeComponent();
+
+        }
+
+
+        private void SrcButton_Click(object sender, RoutedEventArgs e)
+        {
+          
+            //this will hold the name of source folder
+            string s_folderName = "";
+
+
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult res = dialog.ShowDialog();
+
+            if (res == System.Windows.Forms.DialogResult.OK)
+            {
+                s_folderName = dialog.SelectedPath;
+
+            }
+            else
+            {
+                return;
+            }
+
+            SrcFolder.Text = s_folderName;
+
+            string supportedExtensions = "*.jpg,*.gif,*.png,*.bmp,*.jpe,*.jpeg,*.wmf,*.emf";
+            var files = System.IO.Directory.EnumerateFiles(s_folderName, "*.*", SearchOption.AllDirectories).Where(s => supportedExtensions.Contains(System.IO.Path.GetExtension(s).ToLower()));
+
+            foreach (string filename in files)
+            {
+                lbFiles.Items.Add(filename);
+                fileName.Add(filename);
+            }
+
+            
+            /*try
+            {
+                Parallel.ForEach(files, filename =>
+                    {
+
+                        lbFiles.Items.Add(fileName);
+                        fileName.Add(filename);
+                    }
+                    );
+            }
+            catch(AggregateException ex)
+            {
+                throw ex.Flatten();
+            }*/
+
+            images.Source = (ImageSource)new ImageSourceConverter().ConvertFromString(fileName.ElementAt(m_CurrentItemIndex));
+            CopyLogic.mp_CurrentFile = fileName.ElementAt(m_CurrentItemIndex);
+        }
+
+
+        //Button to select destination folder path
+        private void DesButton_Click(object sender, RoutedEventArgs e)
+        {
+
+
+
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult res = dialog.ShowDialog();
+
+            if (res == System.Windows.Forms.DialogResult.OK)
+            {
+                _dfolderName = dialog.SelectedPath;
+            }
+
+
+            DesFolderName.Text = _dfolderName;
+
+        }
+
+        private void CopyButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            MessageBoxImage icon = MessageBoxImage.Stop;
+            while (DesFolderName.Text == "")
+            {
+                System.Windows.MessageBox.Show("Enter Destination", "Error", MessageBoxButton.OK, icon);
+                return;
+            }
+            CopyLogic obj = new CopyLogic(_dfolderName);
+            CopyLogic.Copy();
+
+        }
+
+        private void Nextbutton_Click(object sender, RoutedEventArgs e)
+        {
+            string currentFile;
+            MessageBoxImage icon = MessageBoxImage.Stop;
+            if (SrcFolder.Text != "" && DesFolderName.Text!="")
+            {
+                if (m_CurrentItemIndex == (fileName.Count - 1))
+                {
+                    m_CurrentItemIndex = 0;
+                }
+                else
+                {
+                    ++m_CurrentItemIndex;
+                }
+                currentFile = fileName.ElementAt(m_CurrentItemIndex);
+
+                images.Source = (ImageSource)new ImageSourceConverter().ConvertFromString(currentFile);
+                CopyLogic.Current(currentFile);
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Please Specify Source and Destination First","Error",MessageBoxButton.OK,icon);
+            }
+        }
+
+        private void PrevButton_Click(object sender, RoutedEventArgs e)
+        {
+            string currentFile;
+            MessageBoxImage icon = MessageBoxImage.Stop;
+            if (SrcFolder.Text != "" && DesFolderName.Text != "")
+            {
+                if (m_CurrentItemIndex == 0)
+                {
+                    m_CurrentItemIndex = fileName.Count - 1;
+                }
+                else
+                {
+                    --m_CurrentItemIndex;
+                }
+                currentFile = fileName.ElementAt(m_CurrentItemIndex);
+
+                images.Source = (ImageSource)new ImageSourceConverter().ConvertFromString(currentFile);
+                CopyLogic.Current(currentFile);
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Please Specify Source and Destination First", "Error", MessageBoxButton.OK, icon);
+            }
+
+        }
+
+
+
+
+
+    }
+}
